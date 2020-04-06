1879bc8784d0d000b3818ee10a99efcff6fe738e
mastlouis <mastlouis@wpi.edu>
2020-04-06 14:25:07 -0700

    All three versions with accuracy

--- homework2_template.py
+++ homework2_template.py
@@ -13,7 +13,10 @@ def reshapeAndAppend1s(faces):
 # Given a vector of weights w, a design matrix Xtilde, and a vector of labels y, return the (unregularized)
 # MSE.
 def fMSE(w, Xtilde, y):
-    return 1 / 5000 * (y - Xtilde.T.dot(w)).T.dot(y - Xtilde.T.dot(w))
+    error = y - Xtilde.T.dot(w)
+    return error.T.dot(error) / (Xtilde.shape[1] * 2)
+    # return 1 / 5000 * (y - Xtilde.T.dot(w)).T.dot(y - Xtilde.T.dot(w))
+    # return  Xtilde.dot(Xtilde.T.dot(w) - y) / Xtilde.shape[1]
 
 # Given a vector of weights w, a design matrix Xtilde, and a vector of labels y, and a regularization strength
 # alpha (default value of 0), return the gradient of the (regularized) MSE loss.
@@ -22,8 +25,10 @@ def gradfMSE (w, Xtilde, y, alpha = 0.):
     temp -= y
     temp = Xtilde.dot(temp)
     temp /= Xtilde.shape[0]
-    # temp2 = w.dot(w)(2*X.shape[0])
-    return temp
+    temp2 = w * w.dot(w)
+    temp2 /= Xtilde.shape[0]
+    temp2 *= alpha
+    return temp + temp2
     # return Xtilde.dot(np.dot(Xtilde.T, w) - y)/Xtilde.shape[0]
 
 # Given a design matrix Xtilde and labels y, train a linear regressor for Xtilde and y using the analytical solution.
@@ -35,21 +40,21 @@ def method1(Xtilde, y):
 # Given a design matrix Xtilde and labels y, train a linear regressor for Xtilde and y using gradient descent on fMSE.
 def method2 (Xtilde, y):
     weights = gradientDescent(Xtilde, y)
-    print("Weights: " + str(weights))
     return weights
 
 # Given a design matrix Xtilde and labels y, train a linear regressor for Xtilde and y using gradient descent on fMSE
 # with regularization.
 def method3 (Xtilde, y):
     ALPHA = 0.1
-    pass
+    weights = gradientDescent(Xtilde, y, ALPHA)
+    return weights
 
 # Helper method for method2 and method3.
 def gradientDescent (Xtilde, y, alpha = 0.):
     # EPSILON = 3e-3  # Step size aka learning rate
-    EPSILON = 3e-4
+    EPSILON = 1e-3
     T = 5000  # Number of gradient descent iterations
-    weights = np.random.randn(Xtilde.shape[0]) * 0.01;  # Standard deviation of 0.01
+    weights = np.random.randn(Xtilde.shape[0]) * 0.01  # Standard deviation of 0.01
     for i in range(T):
         weights = weights - (EPSILON * gradfMSE(weights, Xtilde, y, alpha))
     return weights
@@ -62,11 +67,18 @@ if __name__ == "__main__":
     Xtilde_te = reshapeAndAppend1s(np.load("age_regression_Xte.npy"))
     yte = np.load("age_regression_yte.npy")
 
-    # w1 = method1(Xtilde_tr, ytr)
+    w1 = method1(Xtilde_tr, ytr)
+    print("Method one training: ", fMSE(w1, Xtilde_tr, ytr))
+    print("Method one testing: ", fMSE(w1, Xtilde_te, yte))
+    print(w1)
     w2 = method2(Xtilde_tr, ytr)
-    # w3 = method3(Xtilde_tr, ytr)
+    print("Method two training: ", fMSE(w2, Xtilde_tr, ytr))
+    print("Method two testing: ", fMSE(w2, Xtilde_te, yte))
+    print(w2)
+    w3 = method3(Xtilde_tr, ytr)
+    print("Method three training: ", fMSE(w3, Xtilde_tr, ytr))
+    print("Method three testing: ", fMSE(w3, Xtilde_te, yte))
+    print(w3)
 
     # Report fMSE cost using each of the three learned weight vectors
     # ...
-    fMSE_ = fMSE(w1, Xtilde_tr, ytr)
-    print("Method one: ", fMSE_)
